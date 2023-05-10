961d8f09347e74f65505570e83b41391efa332be
muratozbk <slipstream11@yandex.com>
2023-05-10 11:22:30 +0300

    basics

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
@@ -13,6 +10,8 @@ root.render(
     </React.StrictMode>
 );
 console.log(ReactDOM)
+
+// root.render(<App />)
 
 // ReactDOM.render(
 //     <React.StrictMode>
