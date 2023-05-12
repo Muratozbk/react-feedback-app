01bd5484cb1ddfc8b96b8627d927aec6bb32bacd
muratozbk <slipstream11@yandex.com>
2023-05-12 18:40:47 +0300

    media css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackStats from "./components/FeedbackStats";
 import FeedbackData from './data/feedbackData';
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -38,6 +39,7 @@ function App() {
                     ></Route>
                     <Route path='/about' element={<AboutPage />} />
                 </Routes>
+                <AboutIconLink />
             </div>
         </Router >
     )
