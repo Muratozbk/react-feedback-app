76686dd4977d9c30cb76f62e2bba06edad77969b
muratozbk <slipstream11@yandex.com>
2023-05-13 12:40:58 +0300

    feedback context

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,29 +1,14 @@
-import PropTypes from 'prop-types'
-import FeedbackItem from './FeedbackItem'
 import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
+import FeedbackItem from './FeedbackItem'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+    const { feedback } = useContext(FeedbackContext)
+
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback Yet</p>
     }
-
-    // return <div className="feedback-list">
-    //     <AnimatePresence>
-    //         {feedback.map((item) => (
-    //             <motion.div
-    //                 key={item.id}
-    //                 initial={{ opacity: 0 }}
-    //                 animate={{ opacity: 1 }}
-    //                 exit={{ opacity: 0 }}
-    //                 layout
-    //             >
-    //                 <FeedbackItem key={item.id} item={item}
-    //                     handleDelete={handleDelete} />
-    //             </motion.div>
-    //         ))}
-    //     </AnimatePresence>
-    // </div>
-
     return <div className='feedback-list'>
         <AnimatePresence>
             {feedback.map((item) => (
@@ -33,13 +18,11 @@ function FeedbackList({ feedback, handleDelete }) {
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                 >
-                    <FeedbackItem key={item.id} item={item}
-                        handleDelete={handleDelete} />
+                    <FeedbackItem key={item.id} item={item} />
                 </motion.div>
             ))}
         </AnimatePresence>
     </div>
-
     // return <div className="feedback-list">
     //     {feedback.map((item) => (
     //         <FeedbackItem key={item.id} item={item}
@@ -48,14 +31,7 @@ function FeedbackList({ feedback, handleDelete }) {
     // </div>
 }
 
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            // id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired
-        }))
-}
-
 export default FeedbackList
+
+
 
